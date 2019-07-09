94237b3fc0d9a2d62d349a0187c0dc46203b8ba5
eljoy <invimind@gmail.com>
2019-07-10 01:55:16 +0500

    Added Hr element

--- app/core/index.js
+++ app/core/index.js
@@ -10,6 +10,13 @@ export { default as Block } from "./components/layout/Block";
 export { default as Layout } from "./components/layout/Layout";
 export { default as AppBackground } from "./components/layout/AppBackground";
 
+export { default as Api } from "./types/Api";
+export { default as Children } from "./types/Children";
+export { default as Playlist } from "./types/Playlist";
+export { default as Theme } from "./types/Theme";
+export { default as PlaylistDetailsType } from "./types/PlaylistDetailsType";
+export { TrackType } from "./types/PlaylistDetailsType";
+
 export { default as AppText } from "./components/Typography/AppText";
 export { default as Header } from "./components/Typography/Header";
 export { default as Title } from "./components/Typography/Title";
@@ -21,3 +28,4 @@ export { ApiProvider, withApi } from "./services/ApiProvider";
 
 export { default as PlayButton } from "./components/buttons/PlayButton";
 export { default as SplashScreen } from "./components/SplashScreen";
+export { default as Hr } from "./components/Hr";
